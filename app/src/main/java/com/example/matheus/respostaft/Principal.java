679a975a6d33fde72f76d0f9eb831c38bec9ef13
Matheus Rodrigues <matheus.rodrigues@alunos.ifsuldeminas.edu.br>
2018-06-23 00:31:57 -0300

    Geração de graficos de 1 e 2 grau

--- app/src/main/java/com/example/matheus/respostaft/Principal.java
+++ app/src/main/java/com/example/matheus/respostaft/Principal.java
@@ -1,9 +1,11 @@
 package com.example.matheus.respostaft;
 
 import android.app.Activity;
+import android.app.AlertDialog;
 import android.os.Bundle;
 import android.view.Gravity;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.LinearLayout;
@@ -14,11 +16,14 @@ import com.jjoe64.graphview.GraphView;
 import com.jjoe64.graphview.series.DataPoint;
 import com.jjoe64.graphview.series.LineGraphSeries;
 
+import org.apache.commons.math3.analysis.solvers.LaguerreSolver;
+import org.apache.commons.math3.complex.Complex;
 import org.w3c.dom.Text;
 
 import java.util.ArrayList;
 
-public class Principal extends Activity implements View.OnClickListener{
+public class Principal extends Activity implements View.OnClickListener {
+    private LaguerreSolver solver;
     private GraphView grafico;
     private Spinner grauNumerador;
     private Spinner grauDenominador;
@@ -29,21 +34,23 @@ public class Principal extends Activity implements View.OnClickListener{
     private Button geragrafico;
     private ArrayList<EditText> coeficientes;
     private boolean flag;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_principal);
+        solver = new LaguerreSolver();
         flag = false;
         coeficientes = new ArrayList<EditText>();
-        grauNumerador = (Spinner)findViewById(R.id.spinner);
-        grauDenominador = (Spinner)findViewById(R.id.spinner2);
-        sinal = (Spinner)findViewById(R.id.spinner3);
-        ok = (Button)findViewById(R.id.ok);
+        grauNumerador = (Spinner) findViewById(R.id.spinner);
+        grauDenominador = (Spinner) findViewById(R.id.spinner2);
+        sinal = (Spinner) findViewById(R.id.spinner3);
+        ok = (Button) findViewById(R.id.ok);
         ok.setOnClickListener(this);
-        layoutnumerador = (LinearLayout)findViewById(R.id.LayoutNumerador);
-        layoutdenominador = (LinearLayout)findViewById(R.id.LayoutDenominador);
+        layoutnumerador = (LinearLayout) findViewById(R.id.LayoutNumerador);
+        layoutdenominador = (LinearLayout) findViewById(R.id.LayoutDenominador);
         layoutdenominador.removeAllViews();
-        geragrafico = (Button)findViewById(R.id.btngera);
+        geragrafico = (Button) findViewById(R.id.btngera);
         geragrafico.setOnClickListener(this);
         geragrafico.setEnabled(false);
         grafico = (GraphView) findViewById(R.id.graph);
@@ -52,48 +59,79 @@ public class Principal extends Activity implements View.OnClickListener{
 
     @Override
     public void onClick(View view) {
-        if(view == ok){
+        if (view == ok) {
             geragrafico.setEnabled(true);
             grauNumerador.setEnabled(false);
             grauDenominador.setEnabled(false);
             sinal.setEnabled(false);
             ok.setEnabled(false);
-           int graunumerador = 1+grauNumerador.getSelectedItemPosition();
-           int graudenominador = 1+grauDenominador.getSelectedItemPosition();
-           for(int i = graudenominador; i>=0;i--){
-               EditText aux = new EditText(this);
-               aux.setGravity(Gravity.LEFT);
-               coeficientes.add(aux);
-               layoutdenominador.addView(aux);
-               if(i!=0){
-                   TextView coeficiente = new TextView(this);
-                   coeficiente.setGravity(Gravity.LEFT);
-                   coeficiente.setText("S^"+(i)+"+");
-                   layoutdenominador.addView(coeficiente);
-               }
+            int graunumerador = 1 + grauNumerador.getSelectedItemPosition();
+            int graudenominador = 1 + grauDenominador.getSelectedItemPosition();
+            for (int i = graudenominador; i >= 0; i--) {
+                EditText aux = new EditText(this);
+                //aux.setGravity(Gravity.LEFT);
+                coeficientes.add(aux);
+                layoutdenominador.addView(aux);
+                if (i != 0) {
+                    TextView coeficiente = new TextView(this);
+                    coeficiente.setGravity(Gravity.LEFT);
+                    coeficiente.setText("S^" + (i) + "+");
+                    layoutdenominador.addView(coeficiente);
+                }
 
 
-
-           }
+            }
         }
-        if(view == geragrafico){
-            if (flag == false){
-                switch(1+grauDenominador.getSelectedItemPosition()){
+        if (view == geragrafico) {
+            if (flag == false) {
+                switch (1 + grauDenominador.getSelectedItemPosition()) {
                     case 1:
                         grafico.removeAllSeries();
-                        grafico.addSeries(func1grau(Double.parseDouble(coeficientes.get(0).getText().toString()),Double.parseDouble(coeficientes.get(1).getText().toString())));
-                        break;
+                        if(sinal.getSelectedItemPosition() == 0){
+                            grafico.addSeries(func1grau(Double.parseDouble(coeficientes.get(0).getText().toString()), Double.parseDouble(coeficientes.get(1).getText().toString())));
+                            break;
+                        }
+                        else if(sinal.getSelectedItemPosition() == 1){
+                            grafico.addSeries(func2grau(Double.parseDouble(coeficientes.get(0).getText().toString()), Double.parseDouble(coeficientes.get(1).getText().toString()),0));
+                            break;
+                        }
+                        else{
+                            grafico.addSeries(func3grau(Double.parseDouble(coeficientes.get(0).getText().toString()), Double.parseDouble(coeficientes.get(1).getText().toString()),0,0));
+                            break;
+                        }
+
                     case 2:
                         grafico.removeAllSeries();
-                        grafico.addSeries(func2grau(Double.parseDouble(coeficientes.get(0).getText().toString()),Double.parseDouble(coeficientes.get(1).getText().toString()),Double.parseDouble(coeficientes.get(2).getText().toString())));
-                        break;
+                        if(sinal.getSelectedItemPosition() == 0){
+                            grafico.addSeries(func2grau(Double.parseDouble(coeficientes.get(0).getText().toString()), Double.parseDouble(coeficientes.get(1).getText().toString()), Double.parseDouble(coeficientes.get(2).getText().toString())));
+                            break;
+                        }
+                        else if(sinal.getSelectedItemPosition() == 1){
+                            grafico.addSeries(func3grau(Double.parseDouble(coeficientes.get(0).getText().toString()), Double.parseDouble(coeficientes.get(1).getText().toString()), Double.parseDouble(coeficientes.get(2).getText().toString()),0));
+                            break;
+                        }
+                        else{
+                            AlertDialog.Builder builder = new AlertDialog.Builder(this);
+                            builder.setTitle("Erro");
+                            builder.setMessage("Deve Escolher entre Impulso e Degrau");
+                        }
+
                     case 3:
+                        grafico.removeAllSeries();
+                        if(sinal.getSelectedItemPosition() == 0){
+                            grafico.addSeries(func3grau(Double.parseDouble(coeficientes.get(0).getText().toString()), Double.parseDouble(coeficientes.get(1).getText().toString()), Double.parseDouble(coeficientes.get(2).getText().toString()), Double.parseDouble(coeficientes.get(3).getText().toString())));
+                            break;
+                        }
+                        else{
+                            AlertDialog.Builder builder = new AlertDialog.Builder(this);
+                            builder.setTitle("Erro");
+                            builder.setMessage("Para função de 3 grau so e permitido impulso");
+                        }
                         break;
                 }
                 flag = true;
                 geragrafico.setText("Reiniciar");
-            }
-            else{
+            } else {
                 geragrafico.setText("Gerar Grafico");
                 geragrafico.setEnabled(false);
                 coeficientes.removeAll(coeficientes);
@@ -107,45 +145,66 @@ public class Principal extends Activity implements View.OnClickListener{
 
 
         }
+
     }
-    private LineGraphSeries<DataPoint> func1grau(double a,double b){
+
+    private LineGraphSeries<DataPoint> func1grau(double a, double b) {
         DataPoint[] dados = new DataPoint[1000];
         double x = 0;
-        if(a == 1 && b!=0){
-            for(int i = 0; i<1000; i++){
-                x = x +0.001;
-                DataPoint d = new DataPoint(x,Math.pow(Math.E,-1*b*x));
+        if (a == 1 && b != 0) {
+            for (int i = 0; i < 1000; i++) {
+                x = x + 0.001;
+                DataPoint d = new DataPoint(x, Math.pow(Math.E, -1 * b * x));
                 dados[i] = d;
             }
         }
         LineGraphSeries<DataPoint> resposta = new LineGraphSeries<DataPoint>(dados);
         return resposta;
     }
-    private LineGraphSeries<DataPoint> func2grau(double a,double b, double c){
-        double x1,x2;
-        DataPoint[] dados = new DataPoint[500];
-        x1 = (-b + Math.sqrt(b*b-(4*a*c)))/(2*a);
-        x2 = (-b - Math.sqrt(b*b-(4*a*c)))/(2*a);
-        if(x1 != x2){
-            System.out.println("e diferente");
-            double x = 0;
-            double A = 1/(-1*x1+x2);
-            double B=  1/(-1*x2+x1);
-            for(int i = 0; i<500; i++){
-                x = x+0.01;
-                DataPoint d = new DataPoint(x,A*(Math.pow(Math.E,-1*x1*x))+B*(Math.pow(Math.E,-1*x2*x)));
-                System.out.println(Math.pow(Math.E,-1*x1*x)+Math.pow(Math.E,-1*x2*x));
-                dados[i]=d;
+
+    private LineGraphSeries<DataPoint> func2grau(double a, double b, double c) {
+        double x1, x2;
+        DataPoint[] dados = new DataPoint[700];
+        double delta = b * b - (4 * a * c);
+        if (delta >= 0) {
+            x1 = (-b + Math.sqrt(b * b - (4 * a * c))) / (2 * a);
+            x2 = (-b - Math.sqrt(b * b - (4 * a * c))) / (2 * a);
+            if (x1 != x2) {
+                System.out.println("e diferente");
+                double x = 0;
+                System.out.println("X1:"+x1+"X2:"+x2);
+                x1 = x1*(-1);
+                x2 = x2*(-1);
+                double A = 1 / ((-1*x1)+x2);
+                double B = 1 / ((-1*x2)+ x1);
+                System.out.println("A:"+A+"B:"+B);
+                for (int i = 0; i < 700; i++) {
+                    x = x + 0.01;
+                    DataPoint d = new DataPoint(x, A * (Math.pow(Math.E, -1 * x1 * x)) + B * (Math.pow(Math.E, -1 * x2 * x)));
+                    dados[i] = d;
+                }
+                LineGraphSeries<DataPoint> resposta = new LineGraphSeries<DataPoint>(dados);
+                return resposta;
+            } else {
+                double x = 0;
+                for (int i = 0; i < 700; i++) {
+                    x = x + 0.01;
+                    DataPoint d = new DataPoint(x, x * Math.pow(Math.E, -1 * x1 * x));
+                    dados[i] = d;
+                }
+                LineGraphSeries<DataPoint> resposta = new LineGraphSeries<DataPoint>(dados);
+                return resposta;
+
             }
-            LineGraphSeries<DataPoint> resposta = new LineGraphSeries<DataPoint>(dados);
-            return resposta;
         }
-        else{
+        else {
+            x1 = (b/2);
+            x2 = (Math.sqrt(c));
             double x = 0;
-            for(int i = 0; i<500; i++){
-                x = x+0.01;
-                DataPoint d = new DataPoint(x,x*Math.pow(Math.E,-1*x1*x));
-                dados[i]=d;
+            for (int i = 0; i < 700; i++) {
+                x = x + 0.01;
+                DataPoint d = new DataPoint(x,Math.sin(x2*x)* Math.pow(Math.E, -1 * x1 * x));
+                dados[i] = d;
             }
             LineGraphSeries<DataPoint> resposta = new LineGraphSeries<DataPoint>(dados);
             return resposta;
@@ -153,5 +212,14 @@ public class Principal extends Activity implements View.OnClickListener{
         }
 
     }
+    private LineGraphSeries<DataPoint> func3grau(double a, double b, double c,double d) {
+        double[] aux ={a,b,c,d};
+        Complex[] raizes = solver.solveAllComplex(aux,a);
+        if(raizes[0].getImaginary() == 0 && raizes[1].getImaginary()==0 && raizes[2].getImaginary() == 0){
+
+        }
+        return null;
+    }
+
 
 }
